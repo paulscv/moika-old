3f617eb34d559e7ecc3e94be391ed2590f7a36a0
vol2hv <vol2hv@yandex.ru>
2017-03-24 15:44:02 +0300

    Тест OrdermDetail на уровне Service

--- src/test/java/io/khasang/moika/service/impl/OrdermDetailAccessServiceImplTest.java
+++ src/test/java/io/khasang/moika/service/impl/OrdermDetailAccessServiceImplTest.java
@@ -4,7 +4,9 @@ import io.khasang.moika.config.AppConfig;
 import io.khasang.moika.config.HibernateConfig;
 import io.khasang.moika.config.application.WebConfig;
 import io.khasang.moika.entity.Orderm;
+import io.khasang.moika.entity.OrdermDetail;
 import io.khasang.moika.service.OrdermAccessService;
+import io.khasang.moika.service.OrdermDetailAccessService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,30 +14,35 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
 @ContextConfiguration(classes = {AppConfig.class, WebConfig.class, HibernateConfig.class})
-public class OrdermAccessServiceImplTest {
+public class OrdermDetailAccessServiceImplTest {
     @Autowired
     OrdermAccessService ordermAccessService;
+    @Autowired
+    OrdermDetailAccessService ordermDetailAccessService;
 
     @Test
     public void commonOrderm() throws Exception {
         Orderm orderm = new Orderm("1");
         ordermAccessService.create(orderm);
-        long id = orderm.getId();
-        orderm = new Orderm("2");
-        ordermAccessService.create(orderm);
-        orderm = ordermAccessService.getOrderm("2");
-        orderm = ordermAccessService.get(id);
-        orderm.setNumber("11");
-        ordermAccessService.update(orderm);
-        orderm = new Orderm("33");
-        orderm = ordermAccessService.create(orderm);
-        ordermAccessService.delete(orderm);
-        List<Orderm> l = ordermAccessService.getAll();
+        OrdermDetail detail, temp, temp1;
+        detail = new OrdermDetail(new BigDecimal(1), new BigDecimal(1000));
+        temp1 = ordermDetailAccessService.create(orderm, detail);
+        detail = new OrdermDetail(new BigDecimal(2), new BigDecimal(2000));
+        temp = ordermDetailAccessService.create(orderm, detail);
+        detail = new OrdermDetail(new BigDecimal(3), new BigDecimal(3000));
+        ordermDetailAccessService.create(orderm, detail);
+        List<OrdermDetail> list1 = ordermDetailAccessService.getOrdermDetailForOrderm(orderm);
+        ordermDetailAccessService.delete(orderm, temp);
+        list1 = ordermDetailAccessService.getOrdermDetailForOrderm(orderm);
+        detail = ordermDetailAccessService.get(temp1.getId());
+        detail.setSumOfWork(new BigDecimal(777));
+        ordermDetailAccessService.update(detail);
     }
 
 }
